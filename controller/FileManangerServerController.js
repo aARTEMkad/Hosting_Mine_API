d8fbeb27ae0858d9fa1a54593e1d934045c22422
Artem Karpuk <artemkovel2007@gmail.com>
2024-10-12 21:58:34 +0200

    Create function deleteFile, createFile, createDir, uploadFile, #isDirectoryPath more edit function getIsDerictory and added multer for upload file

--- controller/FileManangerServerController.js
+++ controller/FileManangerServerController.js
@@ -3,6 +3,8 @@ import fs from 'fs'
 const pathBind = "/home/user/minecraft/server/";
 
 
+// Write try catch if there is no files
+
 class FileManagerServerController {
 
     // name/Directory
@@ -17,8 +19,7 @@ class FileManagerServerController {
     // name + folder/file 
     getIsDirectory(req, res) {
         const { name, file } = req.body;
-        const data = fs.statSync(pathBind+name+`/${file}`).isDirectory();
-        
+        const data = this.#isDirectoryPath(name, file);
         
         console.log(data);
         res.json({ data: data});
@@ -38,6 +39,64 @@ class FileManagerServerController {
 
         res.json({msg: "Save succsefuly"});
     }
+
+    // pathBind + name + file
+    deleteFile(req, res) {
+        const { name, file } = req.body;
+        fs.rmSync(pathBind+name+`/${file}`, { recursive: true });
+
+        res.json({ msg: "Delete file"})
+    }
+
+    // pathBind + name + file
+    createFile(req, res) {
+        const { name, file} = req.body;
+
+        if(!fs.existsSync(pathBind + name + `/${file}`)) {
+            fs.writeFileSync(pathBind + name + `/${file}`, '');
+            res.json({ msg: "create file"})    
+        } else {
+            res.json({ msg: "don't create"})
+        }
+    }
+
+    createDir(req, res) {
+        const { name, dir } = req.body;
+
+        if(!fs.existsSync(pathBind + name + `/${dir}`)) {
+            fs.mkdirSync(pathBind + name + `/${dir}`, { recursive: true});
+            res.json({ msg: "create dir"})
+        } else {
+            res.json({ msg: "don't create"})
+        }
+    }
+
+    uploadFile(req, res) {
+        const { name, dir } = req.body;
+        const { path, originalname } = req.file;
+
+        fs.copyFileSync(path, pathBind + name + `${dir != "" ? `/${dir}` : ""}` + `/${originalname}`)
+        fs.rmSync(path);
+
+        res.json({msg: "good"})
+    }
+
+
+
+
+
+
+
+
+    #isDirectoryPath(name, path) {
+        console.log(`test: ${name} - ${path}`);
+        const stats = fs.statSync(pathBind+name+`/${path}`);
+        console.log(stats);
+        const isDirectory = stats.isDirectory()
+        return isDirectory
+    }
+
+
 }
 
 const FileManager = new FileManagerServerController();
--- router/FileManagerServerRouter.js
+++ router/FileManagerServerRouter.js
@@ -1,8 +1,28 @@
 import express from "express";
+import multer from "multer";
 
 import FileManager from "../controller/FileManangerServerController.js";
 
+
+
 const Router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const dest = 'files/';
+        cb(null, dest);
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({   
+    storage: storage, 
+    limits: { fileSize: 20 * 1024 * 1024 },
+});
+
+
 
 Router.get('/fileManager', (req, res) => FileManager.getListFiles(req, res)) 
 
@@ -10,6 +30,14 @@ Router.get('/fileManagerStatus', (req, res) => FileManager.getIsDirectory(req, r
 
 Router.get('/infoFile', FileManager.getInfoFile);
 
-Router.post('/saveFile', FileManager.saveInfoFile);
+Router.put('/saveFile', FileManager.saveInfoFile);
+
+Router.delete('/file', FileManager.deleteFile);
+
+Router.post('/file', (req, res) => FileManager.createFile(req, res));
+
+Router.post('/dir', FileManager.createDir);
+
+Router.post('/file/upload', upload.single('file'), FileManager.uploadFile)
 
 export default Router;  
